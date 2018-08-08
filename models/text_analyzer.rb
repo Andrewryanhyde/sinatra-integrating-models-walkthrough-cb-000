0f4aad59d06b6a6597a84884ac7707e095232e4f
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-08 15:08:06 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -21,8 +21,11 @@ class TextAnalyzer
   end
 
   def most_used_letter
-    sl = text.gsub(/[^a-z]/, '') #gets rid of spaces 
+    sl = text.gsub(/[^a-z]/, '') #gets rid of spaces
     arr = sl.split('')
-    arr1 = arr.uniq 
+    arr1 = arr.uniq
     arr2 = {}
-  end
+
+    arr1.map do |c|
+      arr2[c] = arr.count(c)
+    end

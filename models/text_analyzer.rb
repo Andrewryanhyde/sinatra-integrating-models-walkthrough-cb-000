0a67822fbd3e7d6dff5ef6076af94305398d9681
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-08 15:03:57 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,13 @@
 # Your TextAnalyzer model code will go here.
+
+class TextAnalyzer 
+  attr_reader :text 
+
+  def inititalize(text) 
+    @text = text.downcase 
+  end 
+
+  def count_of_words 
+    words = text.split(" ")
+    words.count 
+  end
